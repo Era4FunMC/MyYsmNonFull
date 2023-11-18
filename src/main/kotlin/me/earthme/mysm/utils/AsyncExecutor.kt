c78b6934708d14b89c1acfa5fd9e3d9e88d3adc4
M2ke4U <79621885+MrHua269@users.noreply.github.com>
2023-11-18 09:06:47 +0800

    Use fixed thread pool instead of cached thread pool for async tasks

--- src/main/kotlin/me/earthme/mysm/utils/AsyncExecutor.kt
+++ src/main/kotlin/me/earthme/mysm/utils/AsyncExecutor.kt
@@ -8,12 +8,12 @@ object AsyncExecutor{
     private val threadId = AtomicInteger()
 
     //TODO Use a fixed thread pool?
-    val ASYNC_EXECUTOR_INSTANCE: ExecutorService = Executors.newCachedThreadPool {
+    val ASYNC_EXECUTOR_INSTANCE: ExecutorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()) {
         val wrapped = Executors.defaultThreadFactory().newThread(it)
         wrapped.name = "MyYSM-Async-Worker-${threadId.getAndIncrement()}"
         wrapped.isDaemon = true
         wrapped.priority = 3
-        return@newCachedThreadPool wrapped
+        return@newFixedThreadPool wrapped
     }
 
     init {
